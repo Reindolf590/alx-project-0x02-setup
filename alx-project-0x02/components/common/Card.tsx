97ccd98abc247899fea6edbbafa2dfa445ef0fb8
Reindolf590 <150713702+Reindolf590@users.noreply.github.com>
2025-06-08 14:24:34 +0000

    Update Card.tsx

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,13 +1,12 @@
-import {type CardProps } from "@/interfaces";
+import { type CardProps } from "@/interfaces";
 
 const Card: React.FC<CardProps> = ({title, content}) => {
   return (
     <div className="rounded-2xl shadow-md p-6 bg-white border border-gray-200 hover:shadow-lg">
       <h2 className="text-xl font-semibold mb-2 text-gray-800">{title}</h2>
       <p className="text-gray-600">{content}</p>
-      
     </div>
   )
 }
 
-export default Card
+export default Card;

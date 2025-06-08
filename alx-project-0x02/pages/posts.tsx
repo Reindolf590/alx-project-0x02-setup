358551997d6477201abae7407563da8cb3b7a76b
Reindolf590 <150713702+Reindolf590@users.noreply.github.com>
2025-06-08 22:13:51 +0000

    Update posts.tsx

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -2,46 +2,49 @@ import Head from 'next/head';
 import Header from '@/components/layout/Header';
 import { PostProps } from '@/interfaces';
 import PostCard from '@/components/common/PostCard';
-import { useEffect, useState } from 'react';
 
-export default function Posts() {
-    const [posts, setPosts] = useState<PostProps[]>([]);
+interface PostsPageProps {
+  posts: PostProps[];
+}
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=9');
-            const data = await res.json();
-            const formattedPosts = data.map((post: any) => ({
-                title: post.title,
-                content: post.body,
-                userId: post.userId,
-            }));
-            setPosts(formattedPosts);
-        };
+export default function Posts({ posts }: PostsPageProps) {
+  return (
+    <>
+      <Head>
+        <title>Posts Page</title>
+      </Head>
+      <Header />
+      <main className="bg-gray-100 min-h-screen p-6">
+        <h1 className="text-3xl font-bold mb-6 text-center">Latest Posts 📝</h1>
 
-        fetchPosts();
-    }, []);
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {posts.map((post, index) => (
+            <PostCard
+              key={index}
+              title={post.title}
+              content={post.content}
+              userId={post.userId}
+            />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+}
 
-    return (
-        <>
-            <Head>
-                <title>Posts Page</title>
-            </Head>
-            <Header />
-            <main className="bg-gray-100 min-h-screen p-6">
-                <h1 className="text-3xl font-bold mb-6 text-center">Latest Posts 📝</h1>
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=9');
+  const data = await res.json();
 
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-                    {posts.map((post, index) => (
-                        <PostCard
-                            key={index}
-                            title={post.title}
-                            content={post.content}
-                            userId={post.userId}
-                        />
-                    ))}
-                </div>
-            </main>
-        </>
-    );
+  const posts: PostProps[] = data.map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
